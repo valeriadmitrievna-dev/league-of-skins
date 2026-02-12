eb754daabe2adfe29b27e72d698ec80687c335af
Ключникова Валерия <vklyuchnikova@atollis.com>
2026-02-12 15:52:32 +0300

    virtualized filter list

--- src/widgets/FilterList.tsx
+++ src/widgets/FilterList.tsx
@@ -1,7 +1,7 @@
 import Skeleton from "@/components/Skeleton";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { useCallback, useRef, type FC } from "react";
+import { useCallback, useEffect, useRef, useState, type FC } from "react";
 import { AutoSizer, CellMeasurer, CellMeasurerCache, List, type ListRowRenderer } from "react-virtualized";
 
 interface FilterListProps {
@@ -12,12 +12,33 @@ interface FilterListProps {
 }
 
 const FilterList: FC<FilterListProps> = ({ items, value, onChange, isLoading }) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [listHeight, setListHeight] = useState(248);
+
   const cacheRef = useRef(
     new CellMeasurerCache({
-      fixedWidth: false,
+      fixedWidth: true,
+      fixedHeight: false,
       defaultHeight: 32,
     }),
   );
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const observer = new ResizeObserver(() => {
+      const container = containerRef.current!.querySelector('[role="row"]');
+      setListHeight(Math.min(container?.clientHeight ?? 248, 248));
+    });
+
+    observer.observe(containerRef.current);
+
+    return () => observer.disconnect();
+  }, [items]);
+
+  useEffect(() => {
+    cacheRef.current.clearAll();
+  }, [items]);
 
   const rowRenderer: ListRowRenderer = useCallback(
     ({ index, key, style, parent }) => {
@@ -28,14 +49,13 @@ const FilterList: FC<FilterListProps> = ({ items, value, onChange, isLoading }) 
           {({ registerChild }) => (
             <div ref={registerChild} style={style}>
               <ToggleGroupItem
-                key={item.value}
                 className="
-                cursor-pointer w-full flex justify-start
-                transition-colors hover:text-foreground
-                hover:bg-neutral-300/50 data-state-on:bg-neutral-300
-                dark:hover:bg-neutral-700/50 dark:data-state-on:bg-neutral-700
-                whitespace-normal text-left py-1 h-fit min-h-8
-              "
+                  cursor-pointer w-full flex justify-start
+                  transition-colors hover:text-foreground
+                  hover:bg-neutral-300/50 data-state-on:bg-neutral-300
+                  dark:hover:bg-neutral-700/50 dark:data-state-on:bg-neutral-700
+                  whitespace-normal text-left py-1 h-fit min-h-8
+                "
                 value={item.value}
                 aria-label={item.label}
               >
@@ -51,7 +71,7 @@ const FilterList: FC<FilterListProps> = ({ items, value, onChange, isLoading }) 
 
   return (
     <div className="bg-neutral-100 dark:bg-neutral-800 px-2 py-2 rounded-md border border-foreground/10">
-      <ScrollArea className="max-h-62 overflow-auto scrollbar">
+      <ScrollArea className="max-h-62 overflow-auto scrollbar" ref={containerRef}>
         <ToggleGroup
           type="single"
           orientation="vertical"
@@ -62,13 +82,12 @@ const FilterList: FC<FilterListProps> = ({ items, value, onChange, isLoading }) 
         >
           {isLoading && <Skeleton count={4} className="bg-neutral-300 dark:bg-neutral-700" />}
 
-          {!isLoading && (
+          {!isLoading && listHeight > 0 && (
             <AutoSizer disableHeight>
               {({ width }) => (
                 <List
                   width={width}
-                  // TODO: dynamic height
-                  height={248}
+                  height={listHeight}
                   deferredMeasurementCache={cacheRef.current}
                   rowHeight={cacheRef.current.rowHeight}
                   rowRenderer={rowRenderer}
