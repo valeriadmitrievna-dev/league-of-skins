bbf044d414f28be2c6049444b9777b5a5271a85c
Ключникова Валерия <vklyuchnikova@atollis.com>
2026-03-05 19:20:37 +0300

    UPLOAD INVENTORY

--- src/components/DownloadFile.tsx
+++ src/components/DownloadFile.tsx
@@ -6,12 +6,14 @@ import { readLocalFile } from "@/shared/utils/readLocalFile";
 import { Spinner } from './ui/spinner';
 import Skeleton from './Skeleton';
 import { cn } from '@/shared/utils/cn';
+import { useTranslation } from 'react-i18next';
 
 interface DownloadFileProps {
   fileName: string;
 }
 
 const DownloadFile: FC<DownloadFileProps> = ({ fileName }) => {
+  const { t } = useTranslation();
   const [file, setFile] = useState<File>();
   const [error, setError] = useState(false);
 
@@ -37,6 +39,7 @@ const DownloadFile: FC<DownloadFileProps> = ({ fileName }) => {
   }, []);
 
   return (
+    <>
     <a
       {...(file ? { href: '/' + file.name, download: file.name } : {})}
       className="my-2 flex items-center border w-fit px-2 py-1 pr-3 rounded-md bg-muted gap-x-2"
@@ -44,9 +47,12 @@ const DownloadFile: FC<DownloadFileProps> = ({ fileName }) => {
       <Icon className={cn('size-7', {'text-muted-foreground': !file})} />
       <div>
         {file ? <Typography.Small>league-of-skins-getter.exe</Typography.Small> : <Skeleton className='h-3.5 w-40' />}
-        {file ? <Typography.Muted className="text-[10px]">36.7 MB</Typography.Muted> : <Skeleton className='h-2 mt-1 w-10' />}
+        {file ? <Typography.Muted className="text-[10px]">{(file.size / 1000 / 1000).toFixed(2)} MB</Typography.Muted> : <Skeleton className='h-2 mt-1 w-10' />}
       </div>
     </a>
+    {!error && !file && (
+      <Typography.Muted>{t('temp.wait_file')}</Typography.Muted>
+    )}</>
   );
 };
 
