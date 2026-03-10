ee8e5b328f372296f200187b58ddbb38b2a90e68
Valeria Klyuchnikova <vklyuchnikova@atollis.com>
2026-03-10 15:22:42 +0300

    fix build

--- src/pages/DetailsWishlistPage.tsx
+++ src/pages/DetailsWishlistPage.tsx
@@ -1,15 +1,15 @@
-import { appAuthSelector } from "@/store";
+// import { appAuthSelector } from "@/store";
 import type { FC } from "react";
-import { useTranslation } from "react-i18next";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router";
+// import { useTranslation } from "react-i18next";
+// import { useSelector } from "react-redux";
+// import { useNavigate } from "react-router";
 
 const DetailsWishlistPage: FC = () => {
   // TODO: DetailsSkinPage my-card
-  const navigate = useNavigate();
-  const { t, i18n } = useTranslation();
+  // const navigate = useNavigate();
+  // const { t, i18n } = useTranslation();
 
-  const isAuth = useSelector(appAuthSelector);
+  // const isAuth = useSelector(appAuthSelector);
 
   return (
     <div className="grid grid-cols-[320px_1fr] gap-x-4">
